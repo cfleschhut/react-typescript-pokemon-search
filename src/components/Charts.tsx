3656c3a0f5da8c3295dd959de10e10ea98ad90f6
Christian Fleschhut <weltrekordhalter@gmail.com>
2020-02-10 00:57:58 +0100

    Add responsive line chart

--- src/components/Charts.tsx
+++ src/components/Charts.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import { LineChart, Line, ResponsiveContainer } from 'recharts';
+import * as dataSource from '../data';
+import './Charts.css';
 
-const Charts = () => <h1>Charts</h1>;
+const TinyLineChart = () => (
+  <ResponsiveContainer>
+    <LineChart data={dataSource.d1}>
+      <Line type="monotone" dataKey="pv" stroke="#8884d8" strokeWidth={1} />
+      <Line type="monotone" dataKey="uv" stroke="#82ca9d" strokeWidth={1} />
+    </LineChart>
+  </ResponsiveContainer>
+);
+
+const Charts = () => (
+  <div style={{ width: '100%', height: 300 }}>
+    <h1>Charts</h1>
+
+    <TinyLineChart />
+  </div>
+);
 
 export { Charts };
---
+++ src/data/index.js
@@ -0,0 +1,4 @@
+import d1 from './data01';
+import d2 from './data02';
+
+export { d1, d2 };
